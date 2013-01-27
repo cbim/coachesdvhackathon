a946624621a8bfb01317141600fc96dc0c11065a
Prashant <busybee@gmail.com>
2013-01-27 10:28:24 -0500

    Added a menu that slides out from the left.
    The links in the menu are used to jump to individual slides.

---
+++ assets/themes/coaches/js/slideControls.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+    $('a.showMenu').toggle(function() {
+      $('#menu').animate({left: '16em'}, 300, function() {
+        $('.iosSlider').iosSlider('lock');
+      });
+    }, function() {
+      $('#menu').animate({left: 0}, 300, function() {
+        $('.iosSlider').iosSlider('unlock');
+      });
+    });
+
+    $('.slideSelectors .item a').click(function() {
+      var num = $(this).data('slide');
+      $('.iosSlider').iosSlider('goToSlide', num);
+      $('a.showMenu').click();
+      return false;
+    });
+     
+
+  $('.iosSlider').iosSlider({
+    desktopClickDrag: true,
+    snapToChildren: true,
+    infiniteSlider: true,
+    navSlideSelector: '.sliderContainer .slideSelectors .item',
+    autoSlide: false,
+    scrollbar: true,
+    scrollbarContainer: '.sliderContainer .scrollbarContainer',
+    scrollbarMargin: '0',
+    scrollbarBorderRadius: '0',
+    keyboardControls: true
+  });
+
+});
