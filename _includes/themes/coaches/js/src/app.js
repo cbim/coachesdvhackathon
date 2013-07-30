b1bd081cfdde168d256fc6304d621795f3329be1
Meghna Kukreja <meghna@juiceanalytics.com>
2013-07-30 15:11:08 -0400

    Add the menu to the page.

--- _includes/themes/coaches/js/src/app.js
+++ _includes/themes/coaches/js/src/app.js
@@ -9,10 +9,14 @@
 
   App.init = function() {
     var cards = new App.Collections.Cards();
-    var cardsView = new App.Views.Cards({collection: cards});
-    cardsView.render();
+
+    var menu =  new App.Views.Menu({ collection: cards });
+    $('.menu-container').append(menu.render().el);
+
+    var cardsView = new App.Views.Cards({ collection: cards });
+    menu.$el.append(cardsView.render().el);
+
     cards.fetch();
-    $('#menu').append(cardsView.render().el);
   };
 
   window.app = {init: App.init};
